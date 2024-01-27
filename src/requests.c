d4bf839ae0aa32aed3e3f05e9355fffdced02aa7
ParsaAminpour <parsa.aminpour@gmail.com>
2024-01-27 09:46:17 +0330

    adding the external requests functionality to the project

--- src/requests.c
+++ src/requests.c
@@ -1,7 +1,42 @@
 #include "config.h"
 
-int request(char *url)
+// I had to got this Regex pattern from the StackOverflow. We could have a better Regex pattern.
+const char *re_pattern = "^(https?|ftp)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
+
+const char* _getRegexError(int code) {
+ 	switch(code) {
+    	case REG_NOMATCH: return "No match";
+    	case REG_ECOLLATE: return "Invalid collating element";
+    	case REG_ECTYPE: return "Invalid character class";
+    	case REG_EESCAPE: return "Trailing backslash";
+  	}
+  	return "Unknown regex error"; 
+}
+
+
+bool _validate_url(const char *_url, const char *_re_pattern) {
+  	regex_t regex;
+  	int result;
+
+  	result = regcomp(&regex, _re_pattern, REG_EXTENDED);
+
+  	if (result != 0) {
+    	fprintf(stderr, "%s\n", _getRegexError(result));
+    	return false;
+ 	}
+
+  	result = regexec(&regex, _url, 0, NULL, 0);
+  	regfree(&regex);
+  
+  	return result == 0; 
+}
+
+
+void request(char *_url)
 {
+	bool validate_url = _validate_url(_url, re_pattern);
+	if (!validate_url) exit(-1);
+
 	// Stores the CURL handle used to manage the request and easy API session
   	CURL *curl;
 
@@ -14,10 +49,10 @@ int request(char *url)
   	if (curl == NULL)
   	{
     	fprintf(stderr, "HTTP request failed\n");
-    	return -1;
+    	exit(-1);
   	}
 
-  	curl_easy_setopt(curl, CURLOPT_URL, url);
+  	curl_easy_setopt(curl, CURLOPT_URL, _url);
   
   	// result will store in a string.
   	// If the request is OK we'll get back CURLE_OK as a return value
@@ -25,30 +60,21 @@ int request(char *url)
   
   	if (result != CURLE_OK)
   	{
-    	fprintf(stderr, "Error: %s\n", curl_easy_strerror(result));
-    	return -1;
+    	fprintf(stderr, "Error: %s\n", curl_easy_strerror(result));	
+    	exit(-1);
  	}
-       
+    
+    // printf("%s\n", result);
 	// We call curl_easy_cleanup() to complete/close the session, just like websockets.
 	curl_easy_cleanup(curl);
  
-	return 0;
+	return;
 }
 
 
-int _url_verification(char *_url) {
-	regex_t reegex;
-
-	int compare_result;
-
-	char re_pattern[SIZE] = "(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})(\.[a-zA-Z0-9]{2,})?";
-	compare_result = regcomp(&reegex, re_pattern, 0);
-
-	return compare_result;
-}
-
-
+// NOTE: This is just a sample of the requests.c workflow and should be deleted in the production phase.
 int main() {
 	char *url = (char*) malloc(SIZE * sizeof(char));
-	return 0;
+	scanf("%s", url);
+	request("https://www.google.com");
 }
