84c46604058be5cb0c05440455da5a85a0d12f0b
ParsaAminpour <parsa.aminpour@gmail.com>
2024-01-28 15:30:08 +0330

    just a commit

--- src/debug.c
+++ src/debug.c
@@ -1,4 +1,7 @@
 #include "config.h"
+#include <json-c/json.h>  
+
+//// TEST FILE, IT'S NOT USEABLE ////
 
 bool machine_mode = false;
 
@@ -12,10 +15,32 @@ void change_mode() {
     return;
 }
 
-int main() {
-	printf("%d\n", machine_mode);
-	change_mode();
-	printf("%d\n", machine_mode);
+// int main() {
+// 	printf("%d\n", machine_mode);
+// 	change_mode();
+// 	printf("%d\n", machine_mode);
 
-	return 0;
+// 	return 0;
+// }
+
+int main(int argc, char **argv) {
+    FILE *fp;
+    char buffer[SIZE];
+
+    struct json_object *parsed_json;
+    struct json_object *username;
+    struct json_object *password;
+
+    fp = fopen("../encrypted_data/data.json", "r");
+    fread(buffer, SIZE, 1, fp);
+    fclose(fp);
+
+    parsed_json = json_tokener_parse(buffer);
+
+    json_object_object_get_ex(parsed_json, "username", &username);
+    json_object_object_get_ex(parsed_json, "password", &password);
+
+    printf("%s\n", json_object_get_string(username));
+
+    return 0;
 }
