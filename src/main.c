59aee41feb40be52d2f45dc01c6355912a6c09ba
Alireza Ahmadi <AlirezaAhmadi1729@gmail.com>
2024-01-29 11:40:51 +0330

    #1 modified "main.c" structure  .but incomplete
    main.c has a lot of bug and incomplete parts.... .

---
+++ src/main.c
@@ -0,0 +1,106 @@
+//###############################################
+//               DRIVER_CODE
+//###############################################
+
+
+//############################################### DECLARATIONS
+
+#include "config.h"
+
+
+int main(void)
+{
+    //##################CLI AND MENU
+    // uwu
+
+
+    //#################INPUT_OUTPUT
+    // uwu
+
+    //#################LOGGING
+    // uwu
+
+
+
+    //#################CONFIG
+
+if(log ==NULL || /*The user wanted to change smth in rotors or plugs*/)://if the log was empty
+//first we have to define the rotors we want to work with
+{
+    printf("LOG file not found\n");
+    Rotor RotorsArr[4] = rotorinit();//reflector is defined in config.c
+    
+    //log area start
+    // uwu
+    //log area end
+
+
+
+    //input area start
+    //uwu
+    //input area end
+
+    for (int j = 0; j < n; j++)
+    {
+        
+        back = pre_reflector(&RotorsArr[0], plugboard(c[i], ArrPlug) - 65, 'a');
+        // printf("%c\n", c[i]);
+        for (i = 1; i < 4; i++)
+        {
+            back = pre_reflector(&RotorsArr[i], back, RotorsArr[i - 1].ArrRotor[RotorsArr[i - 1].Position]);
+        }
+
+        for (i = 2; i > -1; i--)
+        {
+            back = post_reflector(&RotorsArr[i], back);
+        }
+
+        if (back < 0)
+        {
+            back += 26;
+        }
+        else
+        {
+            back %= 26;
+        }
+        return(plugboard(back + 65, ArrPlug));
+    }
+
+    // output area start
+    //uwu
+    //output area end
+
+
+    //logging area start
+    //uwu
+    //logging area end
+
+    //cli area start
+    //uwu
+    //cli area end
+    return 0;
+
+}//END OF FILE
+
+
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
